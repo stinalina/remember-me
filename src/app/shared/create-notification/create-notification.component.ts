46292fc4fb5f2ff529a625c4bc3ebd926e727593
stinalina <38920418+stinalina@users.noreply.github.com>
2025-10-25 08:47:17 +0200

    feat: mvp landing-page

--- src/app/shared/create-notification/create-notification.component.ts
+++ src/app/shared/create-notification/create-notification.component.ts
@@ -1,93 +1,80 @@
-//  import type { RememberMeNotification } from '@/models'
-//   import { computed, ref } from 'vue'
-//   import { QuillEditor } from '@vueup/vue-quill'
-//   import '@vueup/vue-quill/dist/vue-quill.snow.css';
-// import { NotificationService } from '@/services/notification-service';
-
-//   const formRef = ref<HTMLFormElement | null>(null);
-//   const editorRef = ref<InstanceType<typeof QuillEditor> | null>(null);
-//   const subject = ref('');
-//   const content = ref('');
-//   const dateTime = ref('');
-//   const mail = ref('');
-//   const now = ref(getNextDay());
-
-//   const sendingNotification = ref(false);
-
-//   function getNextDay() {
-//     const tomorrow = new Date()
-//     const pad = (n: number) => n.toString().padStart(2, '0')
-//     return (
-//       tomorrow.getFullYear() +
-//       '-' +
-//       pad(tomorrow.getMonth() + 1) +
-//       '-' +
-//       pad(tomorrow.getDate() + 1) +
-//       'T' +
-//       '00:30'
-//     )
-//   }
-
-//   const canSubmitForm = computed(() =>  {
-//     return content.value.length > 0 &&
-//       dateTime.value.length > 0 &&
-//       mail.value.length > 0
-//   });
-
-//   function onSubmit() {
-//     if (formRef.value && !formRef.value.checkValidity()) {
-//       formRef.value.reportValidity();
-//       return
-//     }
-
-//     subject.value ??= 'RememberMe Erinnerung';
-//     const notification = {
-//       subject: subject.value,
-//       content: content.value.trim(),
-//       dateTime: dateTime.value,
-//       mail: mail.value
-//     } satisfies RememberMeNotification;
-
-//     sendingNotification.value = true;
-//     NotificationService.getInstance().sendNotification().then(() => {
-//       console.log(JSON.stringify(notification));
-//       resetForm();
-//     }).catch((err: unknown) => {
-//       console.error('Error sending notification:', err);
-//     }).finally(() => {
-//       sendingNotification.value = false;
-//     });
-//   }
-
-//   function resetForm() {
-//     subject.value = '';
-//     content.value = '';
-//     dateTime.value = '';
-//     mail.value = '';
-//     formRef.value?.reset();
-//     editorRef.value?.setText('');
-//   }
-
 import { NgxEditorComponent, NgxEditorMenuComponent, Editor } from 'ngx-editor';
-import { FormsModule } from '@angular/forms';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, computed, inject, OnDestroy, OnInit, signal, viewChild } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { NotificationService } from '../services/notification.service';
 
 @Component({
   selector: 'reme-create-notification',
   templateUrl: 'create-notification.component.html',
   styles: [],
   standalone: true,
-  imports: [NgxEditorComponent, NgxEditorMenuComponent, FormsModule],
+  imports: [NgxEditorComponent, NgxEditorMenuComponent,
+    ReactiveFormsModule] // to get access to FormGroup and the formControlName directive.
 })
-export class CreateNotificationComponent implements OnInit, OnDestroy {
-  html = '';
-  editor!: Editor;
+export class CreateNotificationComponent implements OnDestroy {
 
-  ngOnInit(): void {
-    this.editor = new Editor();
+  private readonly notificationService = inject(NotificationService);
+  private readonly fb = inject(FormBuilder);
+  
+  protected readonly myForm = this.fb.group({
+    subject: [''],
+    content: ['', Validators.required],
+    mail: ['', [Validators.required, Validators.email]],
+    dateTime: [this.getNextDay(), Validators.required],
+  });
+
+  protected readonly formStatus = toSignal(this.myForm.statusChanges, {
+    initialValue: this.myForm.status,
+  });
+  public readonly canSubmitForm = computed(() =>  {
+    return this.formStatus() === 'VALID';
+  });
+
+  protected readonly now = this.getNextDay();
+  protected readonly sendingNotification = signal<boolean>(false);
+  protected readonly placeholderSubject = 'Greetings from Notify!';
+
+  public editor: Editor = new Editor();
+
+  private getNextDay(): string {
+    const date = new Date();
+    const pad = (n: number) => n.toString().padStart(2, '0')
+    return (
+      date.getFullYear() +
+      '-' +
+      pad(date.getMonth() + 1) +
+      '-' +
+      pad(date.getDate() + 1) +
+      'T' +
+      '00:30'
+    )
   }
 
   ngOnDestroy(): void {
     this.editor.destroy();
+  }
+
+  public sendNotification(): void {
+    if (!this.canSubmitForm()) {
+      return;
+    }
+
+    const notification = {
+      subject: this.myForm.value.subject ||  this.placeholderSubject, // use placeholder if empty
+      content: this.myForm.value.content,
+      dateTime: this.myForm.value.dateTime,
+      mail: this.myForm.value.mail
+    };
+
+    this.sendingNotification.set(true);
+    this.notificationService.sendNotification().then(() => {
+      console.log('Notification sent:', notification);
+      this.myForm.reset();
+    }).catch((err: unknown) => {
+      console.error('Error sending notification:', err);
+    }).finally(() => {
+      this.sendingNotification.set(false);
+    });
   }
 }
--- src/app/shared/services/notification.service.ts
+++ src/app/shared/services/notification.service.ts
@@ -1,38 +1,32 @@
-// import { toast } from "vue3-toastify";
-// import "vue3-toastify/dist/index.css";
+import { Inject, Injectable } from "@angular/core";
 
-// export class NotificationService {
-//   private static instance: NotificationService;
+@Injectable({ providedIn: 'root' })
+export class NotificationService {
 
-//   private constructor() {}
-
-//   public static getInstance(): NotificationService {
-//     if (!NotificationService.instance) {
-//       NotificationService.instance = new NotificationService();
-//     }
-//     return NotificationService.instance;
-//   }
-
-//   public sendNotification(): Promise<void> {
-//     console.log('Sending notification...');
-//     return new Promise((resolve) => {
-//       setTimeout(() => {
-//       try {
-//         toast.success('Congratulations!\nErinnerung erfolgreich erstellt!', {
-//           position: 'top-right',
-//           autoClose: 5000,
-//           hideProgressBar: false,
-//           closeOnClick: true,
-//         });
-//         resolve();
-//       } catch (err: unknown) {
-//         toast.error(`Holy smokes!\n${(err as Error).message}`, {
-//           position: 'top-right',
-//           autoClose: 5000,
-//           hideProgressBar: false,
-//           closeOnClick: true,
-//         });
-//       }}, 2000);
-//     });
-//   }
-// }
+  public sendNotification(): Promise<void> {
+    return new Promise((resolve) => setTimeout(() => {
+      resolve(); //without this the promise never terminates aka resolves
+      console.log('Sending notification...');
+    } , 1000));
+  }
+  }
+    // return new Promise((resolve) => {
+    //   setTimeout(() => {
+    //   try {
+    //     toast.success('Congratulations!\nErinnerung erfolgreich erstellt!', {
+    //       position: 'top-right',
+    //       autoClose: 5000,
+    //       hideProgressBar: false,
+    //       closeOnClick: true,
+    //     });
+    //     resolve();
+    //   } catch (err: unknown) {
+    //     toast.error(`Holy smokes!\n${(err as Error).message}`, {
+    //       position: 'top-right',
+    //       autoClose: 5000,
+    //       hideProgressBar: false,
+    //       closeOnClick: true,
+    //     });
+    //   } catch (err: unknown) {
+    //     toast.error(`Holy smokes!\n${(err as Error).message}`, {
+    //       position: 'top-right',
