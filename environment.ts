aaf9df90360a3e3140c8717f74ef2075be463072
stinalina <38920418+stinalina@users.noreply.github.com>
2026-01-04 08:48:06 +0100

    fix: adjust port due to posrt conflicts

--- environment.ts
+++ environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
 
-  SEND_WELCOME_MAIL_URL: 'http://localhost:7071/api/sendWelcomeMail',
+  SEND_WELCOME_MAIL_URL: 'http://localhost:7072/api/sendWelcomeMail',
 };
