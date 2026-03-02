7403be433ea12ab2a297b7c0e495eacbb5bc3b6f
Stina <stinaboehmig@gmx.de>
2026-03-02 08:20:12 +0100

    feature(NOTIFY-64): adjust rechtliche texte for deployment (#25)
    * refactor: adjust impressum
    
    * docs: refine dsgvo and agb

--- src/app/pages/impressum/impressum.component.ts
+++ src/app/pages/impressum/impressum.component.ts
@@ -3,11 +3,5 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'reme-impressum',
   templateUrl: 'impressum.component.html',
-  styles: [],
-  imports: []
 })
-export class ImpressumComponent {
-  public readonly content = '';
-  public readonly phone = '01234 567890';
-  public readonly mail = 'kontakt@deinewebseite.de';
-}
+export class ImpressumComponent {}
