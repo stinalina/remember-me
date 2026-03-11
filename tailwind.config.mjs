4f96b8f632b301354512da39f8bb09b140c55159
stinalina <38920418+stinalina@users.noreply.github.com>
2026-03-11 17:37:29 +0100

    feat: add theme toggle

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,1 +1,13 @@
-
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['light', 'dark', 'synthwave'],
+  },
+};
