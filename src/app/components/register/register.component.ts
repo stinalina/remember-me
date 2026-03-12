361dec21d662c0fc91c9aebe6d267330d463fd24
Stina <stinaboehmig@gmx.de>
2026-03-12 12:34:31 +0100

    feat(NOTIFY-84): rework for first deployment (#34)
    * feat: remove pricing from home and router
    
    * feat: remove agb. Not needed anymore
    
    * feat: add hint why create note is not available
    
    * feat: remove count of interrested; only allow click once

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { ROUTER_TOKENS } from '@app/app.routes';
 import { CheckboxComponent } from '@app/shared/input/checkbox/checkbox.component';
 import { MailComponent } from '@app/shared/mail/mail.component';
 import { ModalComponent } from '@app/shared/modal/modal.component';
 import { PasswordComponent } from '@app/shared/password/password.component';
 import { TextFrameComponent } from '@app/shared/text-frame/text-frame.component';
-import * as agb from '@assets/text/agb.txt';
 import * as dsgvo from '@assets/text/dsgvo.txt';
 
 @Component({
@@ -20,6 +18,5 @@ import * as dsgvo from '@assets/text/dsgvo.txt';
   ],
 })
 export class RegisterComponent {
-  public readonly AgbText = agb.default;
   public readonly DsgvoText = dsgvo.default;
 }
