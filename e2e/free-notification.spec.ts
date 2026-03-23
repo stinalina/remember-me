6faa0dd0479afebbc56e00e3b4d195418da6dd57
stinalina <38920418+stinalina@users.noreply.github.com>
2026-03-23 16:46:46 +0100

    test: out comment unavailable tests fpr mvp mode

--- e2e/free-notification.spec.ts
+++ e2e/free-notification.spec.ts
@@ -1,13 +1,13 @@
-import { test, expect } from '@playwright/test';
+// import { test, expect } from '@playwright/test';
 
-test.describe('FreeNotificationComponent', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/free-notification');
-    await page.waitForLoadState('networkidle');
-    });
+// test.describe('FreeNotificationComponent', () => {
+//   test.beforeEach(async ({ page }) => {
+//     await page.goto('/free-notification');
+//     await page.waitForLoadState('networkidle');
+//     });
 
-  test('should route to login component', async ({ page }) => {
-    await page.getByText('Melde dich noch heute an!', {exact: true}).click();
-    await expect(page).toHaveURL('/login');
-  });
-});
+//   test('should route to login component', async ({ page }) => {
+//     await page.getByText('Melde dich noch heute an!', {exact: true}).click();
+//     await expect(page).toHaveURL('/login');
+//   });
+// });
