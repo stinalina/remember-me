e0d793a27df97d6d170ee8014c22730dedfe1120
stinalina <38920418+stinalina@users.noreply.github.com>
2026-03-28 07:07:11 +0100

    feat: adjust env files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,8 @@ export const environment = {
   production: false,
 
   HASURA_URL: 'http://localhost:8081/v1/graphql',
+  HASURA_ADMIN_SECRET: 'mysecretkey',
+
   BACKEND_URL: 'http://localhost:7072',
   SEND_WELCOME_MAIL_URL: '/api/sendWelcomeMail',
   SEND_REGISTER_INTERESTED_PARTY_MAIL_URL: '/api/registerInterest',
