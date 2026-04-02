7602f00e807dee955523fa68a050f3d4cc1a8260
Stina <stinaboehmig@gmx.de>
2026-04-02 19:10:00 +0200

    feat(NOTIFY-104): remove interessted btn and display login benefits

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,6 @@ export const environment = {
 
   BACKEND_URL: 'http://localhost:7072',
   SEND_WELCOME_MAIL_URL: '/api/sendWelcomeMail',
-  SEND_REGISTER_INTERESTED_PARTY_MAIL_URL: '/api/registerInterest',
   CONTACT_MAIL: 'stina.boehmig@gmx.de',
   MVP_Mode: true
 };
