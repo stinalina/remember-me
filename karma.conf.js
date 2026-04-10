196e2f3ae97854befe556e3fef4fde74061b0289
stinalina <38920418+stinalina@users.noreply.github.com>
2026-04-10 19:07:34 +0200

    feat: update angular 20 -> 21

--- karma.conf.js
+++ karma.conf.js
@@ -4,12 +4,12 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'], // angular devkit löst die Paths zu den testfiles automatisch auf
+    frameworks: ['jasmine'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-jasmine-html-reporter'),
     ],
     client: {
       jasmine: {
