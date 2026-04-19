f82c1d4ecf0409a348aa3223443833de34d00496
stinalina <38920418+stinalina@users.noreply.github.com>
2026-04-19 18:36:32 +0200

    test add e2e

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -5,12 +5,26 @@ test.describe('HomeComponent', () => {
   const testuserPassword = 'test123';
 
   test.beforeEach(async ({ page }) => {
-    await page.goto('/login');
+    await page.goto('/');
     await page.waitForLoadState('networkidle');
-
+    await page.getByRole('button', { name: 'Dran bleiben' }).click();
     await page.getByTestId('login-reme-mail-input').fill(testuserEmail);
     await page.getByTestId('login-reme-password-input').fill(testuserPassword);
     await page.getByRole('button', { name: 'Einloggen' }).click();
+    await page.waitForURL('**/home');
+  });
+
+  test('should display username in header', async ({ page }) => {
+    await expect(page.getByTestId('home-username')).toHaveText('testuser');
+  });
+
+  test('should display correct notifications count and limit', async ({ page }) => {
+    const range = page.getByTestId('home-notifications-range');
+    const tooltip = page.getByTestId('home-notifications-tooltip');
+
+    await expect(range).toHaveValue('0');
+    await expect(range).toHaveAttribute('max', '5');
+    await expect(tooltip).toHaveAttribute('data-tip', '0 von 5 Erinnerungen erstellt diesen Monat.');
   });
 
   test('logout should navigate to login', async ({ page }) => {
