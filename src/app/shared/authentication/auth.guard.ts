fc8ba20174cad89f049e3da081ff42f484055e38
stinalina <38920418+stinalina@users.noreply.github.com>
2026-04-19 14:41:46 +0200

    fix: reload stay on page

--- src/app/shared/authentication/auth.guard.ts
+++ src/app/shared/authentication/auth.guard.ts
@@ -1,10 +1,16 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { Auth } from '@angular/fire/auth';
 import { ROUTER_TOKENS } from '@app/app.routes';
 import { AuthService } from '@app/shared/authentication/auth.service';
 
 export const authGuard: CanActivateFn = async () => {
-  return inject(AuthService).isAuthenticated() 
-    || inject(Router).createUrlTree([ROUTER_TOKENS.LANDING_PAGE]);
-    // router.navigate gibt ein Promise zurück, was hier nicht gewünscht ist
+  const auth = inject(Auth);
+  const router = inject(Router);
+  const authService = inject(AuthService);
+
+  await auth.authStateReady();
+
+  // router.navigate gibt ein Promise zurück, was hier nicht gewünscht ist
+  return authService.isAuthenticated() || router.createUrlTree([ROUTER_TOKENS.LANDING_PAGE]);
 };
