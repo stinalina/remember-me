56f32ada885e05dc1c90286c7f567b13fd05953a
stinalina <38920418+stinalina@users.noreply.github.com>
2026-05-18 16:20:13 +0200

    feat: add hint when is due past

--- src/app/personal-space/home/notes/notification/notification.component.ts
+++ src/app/personal-space/home/notes/notification/notification.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, output, computed } from '@angular/core';
 import { INotification } from '@app/personal-space/data/notification.model';
 import { ConfirmDialog } from '@app/shared/confirmation-dialog/confirmation.dialog';
 import { SafeHtmlPipe } from '@shared/pipe/safe-html.pipe';
@@ -18,4 +18,9 @@ export class NotificationComponent {
   public readonly notification = input.required<INotification>();
   public readonly deleteNotification = output<void>();
   public readonly editClicked = output<void>();
+
+  protected readonly isDuePast = computed(() => {
+    const today = new Date();
+    return new Date(this.notification().dueDate) < today;
+  });
 }
