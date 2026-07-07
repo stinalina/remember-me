d41df00176ceacdc233cf97a9c4e1a1e1eb0bbdc
stinalina <38920418+stinalina@users.noreply.github.com>
2026-07-07 16:53:08 +0200

    feat: first step add subscribeReleaseMails toggle to settings

--- src/app/personal-space/data/preferences.model.ts
+++ src/app/personal-space/data/preferences.model.ts
@@ -1,8 +1,10 @@
 export interface Preferences {
     avatarName: string;
     defaultMail?: string;
+    subscribeReleaseMails: boolean; //TODO Hasura default value
 }
 
 export const InitialPreferences: Preferences = {
-    avatarName: 'Kingston'
+    avatarName: 'Kingston',
+    subscribeReleaseMails: true,
 };
